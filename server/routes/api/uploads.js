3723afb6cc6bcf94a016bbee891541a84822297a
Ryan O'Connor <rocsteady888@gmail.com>
2019-10-22 22:37:24 -0400

    querying for annual heatmap data

--- server/routes/api/uploads.js
+++ server/routes/api/uploads.js
@@ -3,26 +3,22 @@ const router = express.Router();
 
 const AnnualReport = require("../../models/AnnualReport");
 
-// @route   GET /upload/test
+// @route   GET /uploads/test
 // @desc    Tests upload route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Upload Works" }));
 
-// @route   POST /upload
+// @route   POST /uploads
 // @desc    Upload route
 // @access  Public
 router.post("/", async (req, res) => {
   console.log(req.body.data);
   try {
-    AnnualReport.collection.insertMany(req.body.data, onInsert);
-    const onInsert = (err, docs) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.info("%d objects were successfully stored.", docs.length);
-      }
-    }
-  } catch(err) {
+    await AnnualReport.collection.insertMany(req.body.data, status => {
+
+      res.json(status)
+    });
+  } catch (err) {
     console.error(err)
   }
 });
