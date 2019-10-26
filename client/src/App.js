e733141dc3f053fec47f23e3ce130c20e86e3f2f
Rachel Stemerman <rstemerman@gmail.com>
2019-10-26 08:41:58 -0400

    so many changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { AnnualDataProvider } from "./contexts/AnnualDataContext";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import { AnnualDataProvider } from "./contexts/AnnualDataContext";
 import Navigation from "./components/menus/Navigation";
 import Landing from "./views/Landing";
 import AdminView from "./views/AdminView";
 import OfficerView from "./views/OfficerView";
 import ClinicianView from "./views/ClinicianView";
 import SystemView from "./views/SystemView";
+import Login from "./views/Login";
 
-function App() {
-  return (
-    <AnnualDataProvider>
-      <Router>
-        <Navigation />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/admin-view" component={AdminView} />
-        <Route exact path="/officer-view" component={OfficerView} />
-        <Route exact path="/clinician-view" component={ClinicianView} />
-        <Route exact path="/system-view" component={SystemView} />
-      </Router>
-    </AnnualDataProvider>
-  );
-}
+// redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Navigation />
+      <Route exact path="/" component={Login} />
+      <Route exact path="/admin-view" component={AdminView} />
+      <Route exact path="/officer-view" component={OfficerView} />
+      <Route exact path="/clinician-view" component={ClinicianView} />
+      <Route exact path="/system-view" component={SystemView} />
+    </Router>
+  </Provider>
+);
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,27 +1,30 @@
 const express = require("express");
-const connectDB =require('./config/db');
+const connectDB = require("./config/db");
 const app = express();
 
 //Connect to DB
 connectDB();
 
 // Init middleware
-app.use(express.json({ extended: false, limit:'50mb' }));
+// app.get("/", (req, res) => res.send("API Running"));
+app.use(express.json({ extended: false, limit: "50mb" }));
 
-const users = require('./routes/api/users');
-const auth = require('./routes/api/auth');
-const uploads = require('./routes/api/uploads');
-const reports = require('./routes/api/reports');
+const users = require("./routes/api/users");
+const auth = require("./routes/api/auth");
+const uploads = require("./routes/api/uploads");
+const reports = require("./routes/api/reports");
+const test = require("./routes/api/test");
 
 // Use Routes
-app.use('/users', users);
-app.use('/auth', auth);
-app.use('/uploads', uploads);
-app.use('/reports', reports);
+app.use("/users", users);
+app.use("/auth", auth);
+app.use("/uploads", uploads);
+app.use("/reports", reports);
+app.use("/test", test);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  console.log("In production")
+if (process.env.NODE_ENV === "production") {
+  console.log("In production");
 }
 
 const port = process.env.API_PORT || 5000;
