bca069dff89321f3638b6724b7dce2608cc1a3c0
Rachel Stemerman <rstemerman@gmail.com>
2019-11-14 10:37:56 -0500

    file size change

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ connectDB();
 
 // Init middleware
 // app.get("/", (req, res) => res.send("API Running"));
-app.use(express.json({ extended: false, limit: "100mb" }));
+app.use(express.json({ extended: false, limit: "500mb" }));
 
 const users = require("./routes/api/users");
 const auth = require("./routes/api/auth");
